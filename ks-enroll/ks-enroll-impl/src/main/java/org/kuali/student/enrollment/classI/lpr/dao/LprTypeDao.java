64cbf0dd1ee291d00039b981777672dc3a2280f9
sambitpa <sambitpa@0ac02d80-fb3f-11dc-a574-c57244a850eb>
2011-04-25 23:24:40 +0000

    changing package names in ks-enroll-impl, class I and II services can be  identified by package names now.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21615 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/classI/lpr/dao/LprTypeDao.java
@@ -0,0 +1,10 @@
+package org.kuali.student.enrollment.classI.lpr.dao;
+
+import org.kuali.student.enrollment.classI.lpr.model.LuiPersonRelationTypeEntity;
+import org.kuali.student.enrollment.dao.GenericEntityDao;
+
+/**
+ * @author Igor
+ */
+public class LprTypeDao extends GenericEntityDao<LuiPersonRelationTypeEntity> {
+}
